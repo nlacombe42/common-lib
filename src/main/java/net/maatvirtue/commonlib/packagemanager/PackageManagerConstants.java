963668e8a443738522e60f70afcabedd5d6e86d8
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2016-01-09 19:20:23 -0500

    refactored PackageManager class to break it down to more classes

---
+++ src/main/java/net/maatvirtue/commonlib/packagemanager/PackageManagerConstants.java
@@ -0,0 +1,13 @@
+package net.maatvirtue.commonlib.packagemanager;
+
+import net.maatvirtue.commonlib.ffpdp.FfpdpTagV2;
+
+import java.util.regex.Pattern;
+
+public class PackageManagerConstants
+{
+	public static final String ENVIRONMENT_COMPATIBILITY_SEPERATOR = ",";
+	public static final Pattern PACKAGE_NAME_REGX = Pattern.compile("^[a-z]+[a-z0-9\\-]*$");
+	public static FfpdpTagV2 CURRENT_PACKAGE_FFPDP_TAG = new FfpdpTagV2(2, 1, 1, 0);
+	public static final String PACKAGE_RELATION_SEPERATOR = ",";
+}
